f391ef8e44e006bedf9e5a0d90c1d4c93ea9c8b2
Joshua Medlen <joshua246@gmail.com>
2020-10-24 10:21:46 -0500

    added ability to add animals to database

--- animals/request.py
+++ animals/request.py
@@ -80,13 +80,31 @@ def get_single_animal(id):
 
         return json.dumps(animal.__dict__)
 
-# def create_animal(animal):
-#     last_animal = ANIMALS[-1]
-#     new_id = last_animal.id + 1
-#     animal["id"] = new_id
-#     new_animal = Animal(animal['id'], animal['name'], animal['breed'], animal['status'], animal['location_id'], animal['customer_id'])
-#     ANIMALS.append(new_animal)
-#     return animal
+def create_animal(new_animal):
+    with sqlite3.connect("./kennels.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO Animal
+            ( name, breed, status, location_id, customer_id )
+        VALUES
+            ( ?, ?, ?, ?, ?);
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['location_id'],
+              new_animal['customer_id'], ))
+
+        # The `lastrowid` property on the cursor will return
+        # the primary key of the last thing that got added to
+        # the database.
+        id = db_cursor.lastrowid
+
+        # Add the `id` property to the animal dictionary that
+        # was sent by the client so that the client sees the
+        # primary key in the response.
+        new_animal['id'] = id
+
+
+    return json.dumps(new_animal)
 
 def delete_animal(id):
     with sqlite3.connect("./kennels.db") as conn:
--- kennels.sql
+++ kennels.sql
@@ -1,12 +1,1 @@
-SELECT
-    a.id,
-    a.name,
-    a.breed,
-    a.status,
-    a.location_id,
-    a.customer_id,
-    l.name location_name,
-    l.address location_address
-FROM Animal a
-JOIN Location l
-    ON l.id = a.location_id
+SELECT * FROM Animals ORDER BY id DESC;
